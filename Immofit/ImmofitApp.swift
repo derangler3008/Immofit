f52b334d621d6f79cfd246302e159dcee65fec20
Hai Viet Vu <viethaivu2001@gmail.com>
2026-03-24 09:34:50 +0100

    Initial Commit

---
+++ Immofit/ImmofitApp.swift
@@ -0,0 +1,32 @@
+//
+//  ImmofitApp.swift
+//  Immofit
+//
+//  Created by Viet-Hai Vu on 24.03.26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ImmofitApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ ImmofitTests/ImmofitTests.swift
@@ -0,0 +1,17 @@
+//
+//  ImmofitTests.swift
+//  ImmofitTests
+//
+//  Created by Viet-Hai Vu on 24.03.26.
+//
+
+import Testing
+@testable import Immofit
+
+struct ImmofitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
